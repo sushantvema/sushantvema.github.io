8be6e34d1c9db282bf20254f9e7b0dc7cce3c624
Sushant Vema <sushant.h.vema@berkeley.edu>
2024-07-16 15:35:54 -0700

    Quartz sync: Jul 16, 2024, 3:35 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -31,7 +31,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(
       Component.RecentNotes({
         title: "Recent Notes",
-        limit: 10,
+        limit: 6,
         filter: (f) => f.slug!.startsWith("notes/"),
         linkToMore: "notes/" as SimpleSlug,
       }),
