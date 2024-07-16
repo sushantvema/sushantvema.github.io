72ba8c3a63dad7007789b11f4d061a7971b5cb4e
Sushant Vema <sushant.h.vema@berkeley.edu>
2024-07-16 09:37:24 -0700

    Quartz sync: Jul 16, 2024, 9:37 AM

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -64,7 +64,7 @@ export default {
     },
     error: {
       title: "Not Found",
-      notFound: "Either this page is private or doesn't exist.",
+      notFound: "Either this page is private or doesn't yet exist.",
       home: "Return to Homepage",
     },
     folderContent: {
